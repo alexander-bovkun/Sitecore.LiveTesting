9f9b8d35d21bb769b7484e1967745440aa3c55c1
Alexander Bovkun <alb@sitecore.net>
2015-03-27 13:38:23 +0200

    Updated assembly information for Sitecore.LiveTesting.Tests.

--- src/Sitecore.LiveTesting.Tests/Properties/AssemblyInfo.cs
+++ src/Sitecore.LiveTesting.Tests/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sitecore Live Testing")]
+[assembly: AssemblyTitle("Sitecore Live Testing Tests")]
 [assembly: AssemblyCompany("Sitecore")]
 [assembly: AssemblyProduct("Sitecore Live Testing")]
 [assembly: AssemblyCopyright("Copyright © Sitecore 2015")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
