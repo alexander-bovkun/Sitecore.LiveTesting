3d7b597cf33d7e11f199dce0d92262769da0ae1e
Alexander Bovkun <alb@sitecore.net>
2015-03-27 13:37:30 +0200

    New projects with tests for Sitecore.LiveTesting.Extensions was added.

---
+++ src/Sitecore.LiveTesting.Extensions.Tests/Initialization/HookToInitializationHandlerAdapterTest.cs
@@ -0,0 +1,43 @@
+﻿namespace Sitecore.LiveTesting.Extensions.Tests.Initialization
+{
+  using System;
+  using Sitecore.Events.Hooks;
+  using Sitecore.LiveTesting.Extensions.Initialization;
+  using Xunit;
+
+  /// <summary>
+  /// Defines the test class for <see cref="HookToInitializationHandlerAdapter{T}"/>
+  /// </summary>
+  public class HookToInitializationHandlerAdapterTest
+  {
+    /// <summary>
+    /// Should create new instance of type provided in generic argument and call its initialize method.
+    /// </summary>
+    [Fact]
+    public void ShouldCreateNewInstanceOfTypeProvidedInGenericArgumentAndCallItsInitializeMethod()
+    {
+      Activator.CreateInstance<HookToInitializationHandlerAdapter<Hook>>();
+
+      Assert.True(Hook.Initialized);
+    }
+
+    /// <summary>
+    /// Sample hook class.
+    /// </summary>
+    public class Hook : IHook
+    {
+      /// <summary>
+      /// Gets or sets a value indicating whether initialize method was called or not.
+      /// </summary>
+      public static bool Initialized { get; set; }
+      
+      /// <summary>
+      /// The initialize.
+      /// </summary>
+      public void Initialize()
+      {
+        Initialized = true;
+      }
+    }
+  }
+}
