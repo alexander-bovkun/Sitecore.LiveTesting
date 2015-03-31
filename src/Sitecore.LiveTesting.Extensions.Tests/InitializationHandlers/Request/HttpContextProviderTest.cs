13814db59b2f6011597ef1410bf9b972e8b73070
Alexander Bovkun <alb@sitecore.net>
2015-03-31 14:36:23 +0300

    HttpContextProvider initialization handler was introduced in order to expose HttpContext through its static Current property.

---
+++ src/Sitecore.LiveTesting.Extensions.Tests/InitializationHandlers/Request/HttpContextProviderTest.cs
@@ -0,0 +1,48 @@
+﻿namespace Sitecore.LiveTesting.Extensions.Tests.InitializationHandlers.Request
+{
+  using System;
+  using System.IO;
+  using System.Web;
+  using Sitecore.LiveTesting.Extensions.InitializationHandlers.Request;
+  using Sitecore.LiveTesting.Initialization;
+  using Sitecore.LiveTesting.Requests;
+  using Xunit;
+
+  /// <summary>
+  /// Defines the test class for <see cref="HttpContextProviderTest"/>.
+  /// </summary>
+  public class HttpContextProviderTest
+  {
+    /// <summary>
+    /// Should throw not supported exception for other than request initialization context type of contexts.
+    /// </summary>
+    [Fact]
+    public void ShouldThrowNotSupportedExceptionForOtherThanRequestInitializationContextTypeOfContexts()
+    {
+      HttpContextProvider provider = new HttpContextProvider();
+
+      Assert.ThrowsDelegate action = () => provider.SetInitializationContext(new object());
+
+      Assert.Throws<NotSupportedException>(action);
+    }
+
+    /// <summary>
+    /// Should set and restore http context from initialization context.
+    /// </summary>
+    [Fact]
+    public void ShouldSetAndRestoreHttpContextFromInitializationContext()
+    {
+      HttpContext.Current = null;
+      HttpContext httpContext = new HttpContext(new HttpRequest(string.Empty, "http://url", string.Empty), new HttpResponse(new StringWriter()));
+
+      using (HttpContextProvider provider = new HttpContextProvider())
+      {
+        provider.SetInitializationContext(new RequestInitializationContext(new Request(), new Response()) { HttpContext = httpContext });
+
+        Assert.Equal(httpContext, HttpContext.Current);
+      }
+
+      Assert.Null(HttpContext.Current);
+    }
+  }
+}
---
+++ src/Sitecore.LiveTesting.Extensions/InitializationHandlers/Request/HttpContextProvider.cs
@@ -0,0 +1,87 @@
+﻿namespace Sitecore.LiveTesting.Extensions.InitializationHandlers.Request
+{
+  using System;
+  using System.Web;
+  using Sitecore.LiveTesting.Initialization;
+
+  /// <summary>
+  /// Defines initialization handler that exposes an instance of <see cref="HttpContext"/> from.
+  /// </summary>
+  public class HttpContextProvider : IInitializationContextAware, IDisposable
+  {
+    /// <summary>
+    /// The context to restore.
+    /// </summary>
+    private readonly HttpContext contextToRestore;
+
+    /// <summary>
+    /// The disposed.
+    /// </summary>
+    private bool disposed;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HttpContextProvider"/> class.
+    /// </summary>
+    public HttpContextProvider()
+    {
+      this.contextToRestore = HttpContext.Current;
+    }
+
+    /// <summary>
+    /// Finalizes an instance of the <see cref="HttpContextProvider"/> class. 
+    /// </summary>
+    ~HttpContextProvider()
+    {
+      this.Dispose(false);
+    }
+
+    /// <summary>
+    /// The set initialization context.
+    /// </summary>
+    /// <param name="context">The context.</param>
+    public void SetInitializationContext(object context)
+    {
+      if (context == null)
+      {
+        throw new ArgumentNullException("context");
+      }
+
+      RequestInitializationContext requestInitializationContext = context as RequestInitializationContext;
+
+      if (requestInitializationContext == null)
+      {
+        throw new NotSupportedException(string.Format("Only contexts derived from '{0}' are supported.", typeof(RequestInitializationContext).FullName));
+      }
+
+      HttpContext.Current = ((RequestInitializationContext)context).HttpContext;
+    }
+
+    /// <summary>
+    /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+    /// </summary>
+    public void Dispose()
+    {
+      this.Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Releases unmanaged and - optionally - managed resources.
+    /// </summary>
+    /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+    protected virtual void Dispose(bool disposing)
+    {
+      if (this.disposed)
+      {
+        return;
+      }
+
+      if (disposing)
+      {
+        HttpContext.Current = this.contextToRestore;
+      }
+
+      this.disposed = true;
+    }
+  }
+}
