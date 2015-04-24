36352f4550f0fbe7787e402d124456cccf6370f5
Alexander Bovkun <alb@sitecore.net>
2015-04-24 18:15:40 +0300

    Updated versions.

--- src/publish.ps1
+++ src/publish.ps1
@@ -1,3 +1,3 @@
-&nuget push Sitecore.LiveTesting.0.*.nupkg
-&nuget push Sitecore.LiveTesting.Extensions.0.*.nupkg
-&nuget push Sitecore.LiveTesting.SpecFlowPlugin.0.*.nupkg
+&nuget push Sitecore.LiveTesting.0.3.2.0.nupkg
+&nuget push Sitecore.LiveTesting.Extensions.0.3.1.0.nupkg
+&nuget push Sitecore.LiveTesting.SpecFlowPlugin.0.2.0.0.nupkg
