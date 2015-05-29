66d207eb031e962db8c308e6fc7f71d4f7fdca4a
Alexander Bovkun <alb@sitecore.net>
2015-05-29 14:24:40 +0300

    First version of new configuration system for Sitecore.LiveTesting.SpecFlowPlugin was implemented.

---
+++ src/Sitecore.LiveTesting.SpecFlowPlugin/Config/PluginSection.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.LiveTesting.SpecFlowPlugin.Config
+{
+  using System.Configuration;
+
+  /// <summary>
+  /// The configuration section for SpecFlow plugin.
+  /// </summary>
+  public class PluginSection : ConfigurationSection
+  {
+    /// <summary>
+    /// The tag mappings element name.
+    /// </summary>
+    private const string TagMappingsElementName = "tagmappings";
+
+    /// <summary>
+    /// Gets the tag mappings.
+    /// </summary>
+    [ConfigurationProperty(TagMappingsElementName, IsDefaultCollection = true)]
+    public TagMappingCollection TagMappings
+    {
+      get { return (TagMappingCollection)base[TagMappingsElementName]; }
+    }
+  }
+}
---
+++ src/Sitecore.LiveTesting.SpecFlowPlugin/Config/TagMappingCollection.cs
@@ -0,0 +1,30 @@
+﻿namespace Sitecore.LiveTesting.SpecFlowPlugin.Config
+{
+  using System.Configuration;
+
+  /// <summary>
+  /// Defines the tag mapping collection.
+  /// </summary>
+  [ConfigurationCollection(typeof(TagMapping), CollectionType = ConfigurationElementCollectionType.AddRemoveClearMap)]
+  public class TagMappingCollection : ConfigurationElementCollection
+  {
+    /// <summary>
+    /// Create new configuration element.
+    /// </summary>
+    /// <returns>The <see cref="ConfigurationElement"/>.</returns>
+    protected override ConfigurationElement CreateNewElement()
+    {
+      return new TagMapping();
+    }
+
+    /// <summary>
+    /// Gets configuration element key.
+    /// </summary>
+    /// <param name="element">The element.</param>
+    /// <returns>The key.</returns>
+    protected override object GetElementKey(ConfigurationElement element)
+    {
+      return ((TagMapping)element).Tag;
+    }
+  }
+}
