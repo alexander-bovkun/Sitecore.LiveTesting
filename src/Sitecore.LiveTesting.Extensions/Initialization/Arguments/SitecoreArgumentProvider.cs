28f62e02aa2f018141110f557ada1891e8515e9d
Alexander Bovkun <alb@sitecore.net>
2015-06-03 19:30:28 +0300

    Provider that resolves its arguments from Sitecore Factory was implemented.

---
+++ src/Sitecore.LiveTesting.Extensions/Initialization/Arguments/SitecoreArgumentProvider.cs
@@ -0,0 +1,47 @@
+﻿namespace Sitecore.LiveTesting.Extensions.Initialization.Arguments
+{
+  using System;
+  using System.Linq;
+  using System.Reflection;
+  using Sitecore.Configuration;
+  using Sitecore.LiveTesting.Initialization.Arguments;
+
+  /// <summary>
+  /// The Sitecore argument provider.
+  /// </summary>
+  public class SitecoreArgumentProvider : ArgumentProvider
+  {
+    /// <summary>
+    /// Resolves the value for the parameter.
+    /// </summary>
+    /// <param name="value">The initial value.</param>
+    /// <param name="parameter">The parameter.</param>
+    /// <returns>The new value.</returns>
+    protected override object ResolveValue(object value, ParameterInfo parameter)
+    {
+      SitecoreConfigurationMapAttribute mapping = parameter.GetCustomAttributes(typeof(SitecoreConfigurationMapAttribute)).Cast<SitecoreConfigurationMapAttribute>().SingleOrDefault(map => map.ArgumentType == parameter.ParameterType);
+
+      if (mapping == null)
+      {
+        mapping = parameter.Member.GetCustomAttributes(typeof(SitecoreConfigurationMapAttribute)).Cast<SitecoreConfigurationMapAttribute>().SingleOrDefault(map => map.ArgumentType == parameter.ParameterType);
+      }
+
+      if (mapping == null)
+      {
+        mapping = parameter.Member.ReflectedType.GetCustomAttributes(typeof(SitecoreConfigurationMapAttribute)).Cast<SitecoreConfigurationMapAttribute>().SingleOrDefault(map => map.ArgumentType == parameter.ParameterType);
+      }
+
+      if (mapping == null)
+      {
+        mapping = parameter.Member.ReflectedType.Assembly.GetCustomAttributes(typeof(SitecoreConfigurationMapAttribute)).Cast<SitecoreConfigurationMapAttribute>().SingleOrDefault(map => map.ArgumentType == parameter.ParameterType);
+      }
+
+      if (mapping == null)
+      {
+        return null;
+      }
+
+      return Factory.CreateObject(mapping.ConfigurationNode, true);
+    }
+  }
+}
---
+++ src/Sitecore.LiveTesting.Extensions/Initialization/Arguments/SitecoreConfigurationMapAttribute.cs
@@ -0,0 +1,52 @@
+﻿namespace Sitecore.LiveTesting.Extensions.Initialization.Arguments
+{
+  using System;
+  using Sitecore.Diagnostics;
+
+  /// <summary>
+  /// Defines the class that describes how types are mapped to Sitecore configuration.
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Parameter, AllowMultiple = true)]
+  public sealed class SitecoreConfigurationMapAttribute : Attribute
+  {
+    /// <summary>
+    /// The argument type.
+    /// </summary>
+    private readonly Type argumentType;
+
+    /// <summary>
+    /// The configuration node.
+    /// </summary>
+    private readonly string configurationNode;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SitecoreConfigurationMapAttribute"/> class.
+    /// </summary>
+    /// <param name="argumentType">The argument type.</param>
+    /// <param name="configurationNode">The configuration node.</param>
+    public SitecoreConfigurationMapAttribute([NotNull] Type argumentType, [NotNull] string configurationNode)
+    {
+      Assert.ArgumentNotNull(argumentType, "argumentType");
+      Assert.ArgumentNotNullOrEmpty(configurationNode, "configurationNode");
+
+      this.argumentType = argumentType;
+      this.configurationNode = configurationNode;
+    }
+
+    /// <summary>
+    /// Gets the argument type.
+    /// </summary>
+    public Type ArgumentType
+    {
+      get { return this.argumentType; }
+    }
+
+    /// <summary>
+    /// Gets the configuration node.
+    /// </summary>
+    public string ConfigurationNode
+    {
+      get { return this.configurationNode; }
+    }
+  }
+}
