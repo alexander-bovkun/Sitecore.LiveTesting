8c420e75cc7358ec29d024eb5408244b4fe9940d
Alexander Bovkun <alb@sitecore.net>
2015-07-27 17:54:32 +0300

    First version of managed wrapper for NativeHostedWebCore was implemented.

--- src/Sitecore.LiveTesting.IIS/HostedWebCore.cpp
+++ src/Sitecore.LiveTesting.IIS/HostedWebCore.cpp
@@ -4,18 +4,12 @@
 #include <stdexcept>
 #include <string>
 
-#include "windows.h"
-#include "hwebcore.h"
+#include <windows.h>
+#include <hwebcore.h>
 
-std::wstring GetExpandedPath(const std::wstring& path)
-{
-  DWORD length = ExpandEnvironmentStringsW(path.data(), NULL, 0);
-  std::wstring result(length, 0);
+#include <msclr\marshal_cppstd.h>
 
-  ExpandEnvironmentStringsW(path.data(), const_cast<LPWSTR>(result.data()), length);
-
-  return result;
-}
+#using <mscorlib.dll>
 
 class Library
 {
@@ -37,25 +31,88 @@ class Library
     template<typename TFunctionPointer> TFunctionPointer GetFunction(LPCSTR name) const;
 };
 
-class IISServer {
+class NativeHostedWebCore {
   private:
     Library m_hostedWebCoreLibrary;
     PFN_WEB_CORE_ACTIVATE m_activationFunction;
     PFN_WEB_CORE_SHUTDOWN m_shutdownFunction;
   public:
-    IISServer();
+    NativeHostedWebCore();
 
     void Start(PCWSTR hostConfig, PCWSTR rootConfig, PCWSTR instanceName);
     void Stop(DWORD immediate);
 };
 
-IISServer::IISServer() : m_hostedWebCoreLibrary(GetExpandedPath(L"%windir%\\system32\\inetsrv\\hwebcore.dll").data())
+namespace Sitecore
+{
+  namespace LiveTesting
+  {
+    namespace IIS
+    {
+      public ref class HostedWebCore sealed : System::IDisposable
+      {
+        private:
+          NativeHostedWebCore* m_pHostedWebCore;
+          msclr::interop::marshal_context^ m_marshalContext;
+
+          bool m_disposedUnmanagedResources;
+          bool m_disposedManagedResources;
+        protected:
+          !HostedWebCore();
+        public:
+          HostedWebCore();
+
+          void Start(System::String^ hostConfig, System::String^ rootConfig, System::String^ instanceName);
+          void Stop(System::Boolean immediate);
+
+          ~HostedWebCore();
+      };
+    }
+  }
+}
+
+Sitecore::LiveTesting::IIS::HostedWebCore::HostedWebCore() : m_pHostedWebCore(new NativeHostedWebCore()), m_marshalContext(gcnew msclr::interop::marshal_context())
+{
+}
+
+void Sitecore::LiveTesting::IIS::HostedWebCore::Start(System::String^ hostConfig, System::String^ rootConfig, System::String^ instanceName)
+{
+  m_pHostedWebCore->Start(m_marshalContext->marshal_as<PCWSTR>(hostConfig), m_marshalContext->marshal_as<PCWSTR>(rootConfig), m_marshalContext->marshal_as<PCWSTR>(instanceName));
+}
+
+void Sitecore::LiveTesting::IIS::HostedWebCore::Stop(System::Boolean immediate)
+{
+  m_pHostedWebCore->Stop(immediate);
+}
+
+Sitecore::LiveTesting::IIS::HostedWebCore::~HostedWebCore()
+{
+  if (!m_disposedManagedResources) {
+    m_marshalContext->~marshal_context();
+    m_disposedManagedResources = true;
+  }
+
+  this->!HostedWebCore();
+}
+
+Sitecore::LiveTesting::IIS::HostedWebCore::!HostedWebCore()
+{
+  if (!m_disposedUnmanagedResources)
+  {
+    delete m_pHostedWebCore;
+    m_disposedUnmanagedResources = true;
+  }
+}
+
+std::wstring GetExpandedPath(const std::wstring& path);
+
+NativeHostedWebCore::NativeHostedWebCore() : m_hostedWebCoreLibrary(GetExpandedPath(L"%windir%\\system32\\inetsrv\\hwebcore.dll").data())
 {
   m_activationFunction = m_hostedWebCoreLibrary.GetFunction<PFN_WEB_CORE_ACTIVATE>("WebCoreActivate");
   m_shutdownFunction = m_hostedWebCoreLibrary.GetFunction<PFN_WEB_CORE_SHUTDOWN>("WebCoreShutdown");
 }
 
-void IISServer::Start(PCWSTR hostConfig, PCWSTR rootConfig, PCWSTR instanceName)
+void NativeHostedWebCore::Start(PCWSTR hostConfig, PCWSTR rootConfig, PCWSTR instanceName)
 {
   HRESULT result = m_activationFunction(hostConfig, rootConfig, instanceName);
 
@@ -65,7 +122,7 @@ void IISServer::Start(PCWSTR hostConfig, PCWSTR rootConfig, PCWSTR instanceName)
   }
 }
 
-void IISServer::Stop(DWORD immediate)
+void NativeHostedWebCore::Stop(DWORD immediate)
 {
   HRESULT result = m_shutdownFunction(immediate);
 
@@ -102,5 +159,12 @@ template<typename TFunctionPointer> TFunctionPointer Library::GetFunction(LPCSTR
   }
 }
 
-__declspec(dllexport) int StartHostedWebCore();
-__declspec(dllexport) int StopHostedWebCore();
+std::wstring GetExpandedPath(const std::wstring& path)
+{
+  DWORD length = ExpandEnvironmentStringsW(path.data(), NULL, 0);
+  std::wstring result(length, 0);
+
+  ExpandEnvironmentStringsW(path.data(), const_cast<LPWSTR>(result.data()), length);
+
+  return result;
+}
