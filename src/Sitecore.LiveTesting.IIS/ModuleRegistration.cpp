738a32025726db0a192fea5e7a6aa234568739a8
Alexander Bovkun <alb@sitecore.net>
2015-09-21 20:44:55 +0300

    Warning was disabled for everything coming from <httpserv.h>.

--- src/Sitecore.LiveTesting.IIS/ModuleRegistration.cpp
+++ src/Sitecore.LiveTesting.IIS/ModuleRegistration.cpp
@@ -1,7 +1,11 @@
 #define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>
+
+#pragma warning(push)
+#pragma warning(disable: 6101)
 #include <httpserv.h>
+#pragma warning(pop)
 
 HRESULT RegisterModule(DWORD dwServerVersion, IHttpModuleRegistrationInfo* pModuleInfo, IHttpServer* pHttpServer)
 {
