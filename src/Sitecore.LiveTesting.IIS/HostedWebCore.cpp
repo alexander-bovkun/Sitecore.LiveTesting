80ccfb8370cc48c26ec51020b5903f1427b4392f
Alexander Bovkun <alb@sitecore.net>
2015-10-06 12:04:54 +0300

    Proper native to managed exception conversion was added.

--- src/Sitecore.LiveTesting.IIS/HostedWebCore.cpp
+++ src/Sitecore.LiveTesting.IIS/HostedWebCore.cpp
@@ -20,9 +20,19 @@ System::String^ Sitecore::LiveTesting::IIS::HostedWebCore::CurrentInstanceName::
   return gcnew System::String(NativeHostedWebCore::GetCurrentInstanceName().data());
 }
 
-Sitecore::LiveTesting::IIS::HostedWebCore::HostedWebCore(System::String^ hostedWebCoreLibraryPath, System::String^ hostConfig, System::String^ rootConfig, System::String^ instanceName) : m_marshalContext(gcnew msclr::interop::marshal_context()), m_pHostedWebCore(&NativeHostedWebCore::GetInstance(m_marshalContext->marshal_as<PCWSTR>(hostedWebCoreLibraryPath), m_marshalContext->marshal_as<PCWSTR>(hostConfig), m_marshalContext->marshal_as<PCWSTR>(rootConfig), m_marshalContext->marshal_as<PCWSTR>(instanceName)))
+Sitecore::LiveTesting::IIS::HostedWebCore::HostedWebCore(System::String^ hostedWebCoreLibraryPath, System::String^ hostConfig, System::String^ rootConfig, System::String^ instanceName) try : m_marshalContext(gcnew msclr::interop::marshal_context()), m_pHostedWebCore(&NativeHostedWebCore::GetInstance(m_marshalContext->marshal_as<PCWSTR>(hostedWebCoreLibraryPath), m_marshalContext->marshal_as<PCWSTR>(hostConfig), m_marshalContext->marshal_as<PCWSTR>(rootConfig), m_marshalContext->marshal_as<PCWSTR>(instanceName)))
 {
   m_marshalContext->~marshal_context();
+}
+catch (const std::runtime_error& e)
+{
+  m_marshalContext->~marshal_context();
+  throw gcnew System::InvalidOperationException(gcnew System::String(e.what()));
+}
+catch (const std::invalid_argument& e)
+{
+  m_marshalContext->~marshal_context();
+  throw gcnew System::ArgumentException(gcnew System::String(e.what()));
 }
 
 Sitecore::LiveTesting::IIS::HostedWebCore::HostedWebCore(System::String^ hostConfig, System::String^ rootConfig, System::String^ instanceName) : Sitecore::LiveTesting::IIS::HostedWebCore::HostedWebCore(System::Environment::ExpandEnvironmentVariables("%windir%\\system32\\inetsrv\\hwebcore.dll"), hostConfig, rootConfig, instanceName)
