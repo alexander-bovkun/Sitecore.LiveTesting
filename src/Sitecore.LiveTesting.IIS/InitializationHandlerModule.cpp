7e7e3ce9fdf5f2f26df133bb4b6ebb92acda3473
Alexander Bovkun <alb@sitecore.net>
2015-12-22 21:16:46 +0200

    Introduced global subscription point to HttpApplication BeginRequest and EndRequest events.

--- src/Sitecore.LiveTesting.IIS/InitializationHandlerModule.cpp
+++ src/Sitecore.LiveTesting.IIS/InitializationHandlerModule.cpp
@@ -1,10 +1,25 @@
 #include "InitializationHandlerModule.h"
 
+void Sitecore::LiveTesting::IIS::Requests::InitializationHandlerModule::OnBeginRequest(_In_ System::Object^ sender, _In_ System::EventArgs^ args)
+{
+  BeginRequest(sender, args);
+}
+
+void Sitecore::LiveTesting::IIS::Requests::InitializationHandlerModule::OnEndRequest(_In_ System::Object^ sender, _In_ System::EventArgs^ args)
+{
+  EndRequest(sender, args);
+}
+
 void Sitecore::LiveTesting::IIS::Requests::InitializationHandlerModule::Init(_In_ System::Web::HttpApplication^ application)
 {
-  application;
+  m_application = application;
+  
+  m_application->BeginRequest += gcnew System::EventHandler(this, &InitializationHandlerModule::OnBeginRequest);
+  m_application->EndRequest += gcnew System::EventHandler(this, &InitializationHandlerModule::OnEndRequest);
 }
 
 Sitecore::LiveTesting::IIS::Requests::InitializationHandlerModule::~InitializationHandlerModule()
 {
+  m_application->EndRequest -= gcnew System::EventHandler(this, &InitializationHandlerModule::OnEndRequest);
+  m_application->BeginRequest -= gcnew System::EventHandler(this, &InitializationHandlerModule::OnBeginRequest);
 }
--- src/Sitecore.LiveTesting.IIS/InitializationHandlerModule.h
+++ src/Sitecore.LiveTesting.IIS/InitializationHandlerModule.h
@@ -12,7 +12,15 @@ namespace Sitecore
       {
         public ref class InitializationHandlerModule sealed : public System::Web::IHttpModule
         {
+          private:
+            System::Web::HttpApplication^ m_application;
+
+            void OnBeginRequest(_In_ System::Object^ sender, _In_ System::EventArgs^ args);
+            void OnEndRequest(_In_ System::Object^ sender, _In_ System::EventArgs^ args);
           public:
+            static event System::EventHandler^ BeginRequest;
+            static event System::EventHandler^ EndRequest;
+
             virtual void Init(_In_ System::Web::HttpApplication^ application);
 
             ~InitializationHandlerModule();
