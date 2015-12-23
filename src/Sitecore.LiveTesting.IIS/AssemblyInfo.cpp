cadeaa951c61e14f65e314171becf6fa345a0876
Alexander Bovkun <alb@sitecore.net>
2015-12-23 12:56:52 +0200

    Added AssemblyInfo file for Sitecore.LiveTesting.IIS.

---
+++ src/Sitecore.LiveTesting.IIS/AssemblyInfo.cpp
@@ -0,0 +1,29 @@
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly:System::Reflection::AssemblyTitle("Sitecore Live Testing IIS")];
+[assembly:System::Reflection::AssemblyDescription("Helper library that simplifies the process of running tests in live IIS environment")];
+[assembly:System::Reflection::AssemblyConfiguration("")];
+[assembly:System::Reflection::AssemblyCompany("Sitecore")];
+[assembly:System::Reflection::AssemblyProduct("Sitecore Live Testing")];
+[assembly:System::Reflection::AssemblyCopyright("Copyright © Sitecore 2015")];
+[assembly:System::Reflection::AssemblyTrademark("")];
+[assembly:System::Reflection::AssemblyCulture("")];
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly:System::Runtime::InteropServices::ComVisible(false)];
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly:System::Reflection::AssemblyVersion("0.4.1.0")];
+[assembly:System::Reflection::AssemblyFileVersion("0.4.1.0")];
